ca678414c6ec177eb45738b657909cf855fd2403
Abid-2743 <arabyhossainabid@gmail.com>
2024-12-30 14:10:32 +0600

    new changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from 'daisyui';
+
 export default {
   darkMode: 'class', // Enables dark mode via class strategy
-  content: [
-    './src/**/*.{js,jsx,ts,tsx}', // Ensures Tailwind scans all React files
-  ],
-  
-  daisyui: {
-    themes: [
-      "light",
-      "black",
-    ],
-  },
-  plugins: [
-    require('daisyui'),
-  ],
-}
+  content: ['./src/**/*.{js,jsx,ts,tsx}'], // Ensures Tailwind scans all React files
 
+  theme: {
+    daisyui: {
+      themes: ['light', 'black'],
+    },
+  },
+
+  plugins: [daisyui],
+};
